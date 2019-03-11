7d9ede4d745a6b9546f66eebf40c9f4db9802d11
HongYang Gao <hongyang.gao@gmail.com>
2019-03-10 18:46:00 -0700

    Add modal components and actions

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+import Modal from './modal/modal_container';
 
-import MainPage from './main/main_page';
+import MainPageContainer from './main/main_page_container';
 import Explore from './explore/explore';
 import LoginFormContainer from './session/login_form_container';
 import SignUpFormContainer from './session/signup_form_container';
 
 const App = () => (
   <div>
+    <Modal />
     {/* <NavBarContainer /> */}
     <Switch>
       <ProtectedRoute path='/explore' component={Explore}/>
-      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/" component={MainPageContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
     </Switch>
