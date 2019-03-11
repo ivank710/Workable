190b1ff6077702fa72630ba617b6c67de70514d9
Ivan Kwong <ivan.kwong710@gmail.com>
2019-03-11 11:55:24 -0700

    Style splash page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import '../../css/_main_page.css';
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Workable</h1>
-        <button className='mainsignin' onClick={() => this.props.openModal('login')}>Login</button>
-        <button className='mainsignin' onClick={() => this.props.openModal('signup')}>SignUp</button>
-        <footer>
-          Copyright
-        </footer>
-      </div>
+      <>
+        <div className="splash-container">
+          <div className="splash-title">
+            <h1>Workable</h1>
+          </div>
+        </div>
+          <div className="splash-slogan">
+            <h3>Find your job today!</h3>
+          </div>
+
+        <div className="splash-buttons">
+          <button
+            className="login"
+            onClick={() => this.props.openModal("login")}
+          >
+            {" "}
+            Login
+          </button>
+          &nbsp; &nbsp;&nbsp;
+          <button
+            className="signup"
+            onClick={() => this.props.openModal("signup")}
+          >
+            {" "}
+            Sign Up
+          </button>
+        </div>
+
+        <div className="footer">
+          {/* <footer>Copyright</footer> */}
+        </div>
+      </>
     );  
   }
 }
