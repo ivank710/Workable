f0f22e9c9c9ec37c6369af3193d06ff19656a772
your name here <jhspark17@gmail.com>
2019-03-13 10:19:17 -0700

    Add css to explore page

--- frontend/src/components/explore/job_index_item.jsx
+++ frontend/src/components/explore/job_index_item.jsx
@@ -25,9 +25,9 @@ class JobIndexItem extends React.Component {
     return (
       <div className="content-container">
         <div className="left-content">
-          <div className="content-type-left">Title: {this.props.job.title}</div>
+          <div className="content-type-left"><span className="content-label">Title:</span> {this.props.job.title}</div>
           <div className="content-type-left">
-            Company: {this.props.job.company}
+            <span className="content-label">Company:</span> {this.props.job.company}
           </div>
           <div className="content-type-left">
             <a id="apply-now" href={this.props.job.company_url}>Apply Now</a>
@@ -38,12 +38,8 @@ class JobIndexItem extends React.Component {
         </div>
         <div className="right-content">
           <div className="right-content-top">
-            <div className="content-type-right">
-              Type: {this.props.job.type}
-            </div>
-            <div className="content-type-right">
-              Location: {this.props.job.location}
-            </div>
+            <div className="content-type-right"><span className="content-label">Type:&nbsp;</span>  {this.props.job.type}</div>
+            <div className="content-type-right"> <span className="content-label">Location:&nbsp;</span> {this.props.job.location}</div>
           </div>
           <div >
             <DescriptionItem description={this.props.job.description} />
