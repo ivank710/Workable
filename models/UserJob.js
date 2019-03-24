e8a6a08a1e672257655d2f7f17852258d43b00ad
HongYang Gao <hongyang.gao@gmail.com>
2019-03-24 14:58:23 -0700

    Add user jobs model

---
+++ models/UserJob.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserJobSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    company_logo: {
+        type: String,
+        required: false
+    },
+    company_url: {
+        type: String,
+        required: false
+    },
+    created_at: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    how_to_apply: {
+        type: String,
+        required: false
+    },
+    location: {
+        type: String,
+        required: false
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+
+});
+
+const UserJob = mongoose.model('userJobs', UserJobSchema);
+module.exports = UserJob;
