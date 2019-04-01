5f437ec959a030372b2b25562c4aaabce218bfc4
HongYang Gao <hongyang.gao@gmail.com>
2019-03-31 17:25:35 -0700

    Reformat user page html

--- frontend/src/components/userpage/user_page.jsx
+++ frontend/src/components/userpage/user_page.jsx
@@ -19,20 +19,37 @@ class UserPage extends React.Component {
 
         return(
          <Layout>
-           <div className="sidebar">
-             <div className="avatar"></div>
-              <UploadContainer />
+           <div className='usermain'>
+              <div className="sidebar">
+                <div className="avatar"></div>
+                  <UploadContainer />
+                  <div className='yourkeywords'>Your Keywords:</div>
+                  <div className='keywords'>
+                    <div>keywords</div>
+                    <div>keywords</div>
+                    <div>keywords</div>
+                    <div>keywords</div>
+                    <div>keywords</div>
+                  </div>
+              </div>
+
+              <div className='mainbar'>
+                <div className='mainbartitle'>
+                    My Saved Jobs 
+                </div>
+                <div className='jobitems'>
+                      items
+                </div>
+              </div>
            </div>
-           
+{/*            
           <div className="list-container">
             <div className="my-list">My Saved Jobs</div>
             <div className="list-box">
             <div className="job">vdfv fdz dfs</div>
-            <div className="job"></div>
-            <div className="job"></div>
-            <div className="job"></div>
+          
             </div>
-          </div>
+          </div> */}
          </Layout>
         )
     }
