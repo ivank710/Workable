1889feac167050cc4b5b98d98abe236e26c67e7f
Rayyan <rmahar@ucsd.edu>
2019-04-03 19:10:01 -0700

    Add user page styling

--- frontend/src/components/userpage/user_page.jsx
+++ frontend/src/components/userpage/user_page.jsx
@@ -1,67 +1,73 @@
-import React from 'react';
-import Layout from '../layout/layout';
-import '../../css/_user_page.css';
+import React from "react";
+import Layout from "../layout/layout";
+import "../../css/_user_page.css";
 import UploadContainer from "../../components/explore/upload_container";
-import UserJobItem from '../userpage/userJob_item';
+import UserJobItem from "../userpage/userJob_item";
 
 class UserPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-      super(props);
+  componentDidMount() {
+    this.props.fetchUserJobs();
+    this.props.fetchKeywords();
+  }
+
+  render() {
+    let userJobs = this.props.userJobs;
+
+    let keywords;
+    if (this.props.keywords === undefined) {
+      keywords = "";
+    } else {
+      keywords = this.props.keywords.keywords.map(el => {
+        return <span className="word">&bull; {el}</span>;
+      });
     }
 
-    componentDidMount() {
-      this.props.fetchUserJobs();
-      this.props.fetchKeywords();
-    }
-
-    render() {
-        let userJobs = this.props.userJobs;
-
-        let keywords;
-        if (this.props.keywords === undefined) {
-          keywords = '';
-        } else {
-           keywords = this.props.keywords.keywords.map(el => {
-            return (
-              <span className='word'>&bull; {el}</span>
-            )
-          });
-          
-        }
-
-        return(
-         <Layout>
-           <div className='usermain'>
-              <div className="sidebar">
-                <div className="avatar"></div>
-                  <UploadContainer />
-                  <div className='yourkeywords'>Your Keywords: </div>
-                  <div className='keywords'>
+    return (
+      <Layout>
+        <div className="usermain">
+          <div className="sidebar">
+            <div className="avatar" />
+            <UploadContainer />
+            {/* <div className='keywords'>
                     <div className='singleKeyword'>{keywords[1]}</div>
                     <div className='singleKeyword'>{keywords[2]}</div>
                     <div className='singleKeyword'>{keywords[3]}</div>
                     <div className='singleKeyword'>{keywords[4]}</div>
                     <div className='singleKeyword'>{keywords[5]}</div>
-                  </div>
-              </div>
+                  </div> */}
+            <div class="card-holder">
+              <div class="card bg-gold">
+                <div className="yourkeywords">Your Keywords: </div>
 
-              <div className='mainbar'>
-                <div className='mainbartitle'>
-                    My Saved Jobs 
-                </div>
-                <div className='jobitems'>
-                  <div className="job-item">
-                    {this.props.userJobs.map(jobItem => (
-                      <UserJobItem job={jobItem} deleteJob={this.props.deleteJob} />
-                    ))}
-                  </div>
+                <div className="keywords">
+                  <div className="singleKeyword">{keywords[1]}</div>
+                  <div className="singleKeyword">{keywords[2]}</div>
+                  <div className="singleKeyword">{keywords[3]}</div>
+                  <div className="singleKeyword">{keywords[4]}</div>
+                  <div className="singleKeyword">{keywords[5]}</div>
                 </div>
               </div>
-           </div>
-         </Layout>
-        )
-    }
+            </div>
+          </div>
+
+          <div className="mainbar">
+            <div className="mainbartitle">My Saved Jobs</div>
+            <div className="jobitems">
+              <div className="job-item">
+                {this.props.userJobs.map(jobItem => (
+                  <UserJobItem job={jobItem} deleteJob={this.props.deleteJob} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default UserPage;
